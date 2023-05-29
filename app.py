9ada5bc1fc67f457060601e1bf466fb08041772c
Jakub <email for my second accountjakub.zubek.1998@gmail.com>
2023-05-29 18:45:49 +0200

    making navbar alive

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return (render_template("index.html"))
+    return (render_template("index.html", active_menu="home"))
 
 @app.route("/about")
 def about():
-    return (render_template("about.html"))
+    return (render_template("about.html", active_menu="about"))
 
 @app.route("/activites")
 def activites():
-    return (render_template("activites.html"))
+    return (render_template("activites.html", active_menu="activites"))
 
 @app.route("/creators")
 def creators():
-    return (render_template("creators.html"))
+    return (render_template("creators.html", active_menu="creators"))
 
 
 
